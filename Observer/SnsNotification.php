9f124da660ba163d8d9d89ecfe9c6bfabed04b05
Ammar <ammar@shopgo.me>
2016-02-20 00:16:51 +0200

    Add SNS notification event observer

---
+++ Observer/SnsNotification.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © 2015 ShopGo. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShopGo\Locker\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Sns notification observer
+ */
+class SnsNotification implements ObserverInterface
+{
+    /**
+     * Locker code
+     */
+    const CODE = 'locker';
+
+    /**
+     * Set lock status action
+     */
+    const ACTION_SET_LOCK_STATUS = 'set_lock_status';
+
+    /**
+     * Notifier model
+     *
+     * @var \ShopGo\Locker\Model\Lock
+     */
+    protected $_lock;
+
+    /**
+     * @param \ShopGo\Locker\Model\Lock $lock
+     */
+    public function __construct(\ShopGo\Locker\Model\Lock $lock)
+    {
+        $this->_lock = $lock;
+    }
+
+    /**
+     * Handle SNS notifications
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $notification = $observer->getEvent()->getData('notification');
+
+        if ($notification['module'] != self::CODE) {
+            return false;
+        }
+
+        switch ($notification['action']) {
+            case self::ACTION_SET_LOCK_STATUS:
+                $this->_notification->setLockStatus(
+                    $notification['arguments']['lock_status']
+                );
+                break;
+        }
+    }
+}
